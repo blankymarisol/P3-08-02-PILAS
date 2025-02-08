eaf0919cc27cbc1697dac5844628ab5d62d7b4e2
blankymarisol <blopezm50@miumg.edu.gt>
2025-02-08 12:39:21 -0600

    CODIGO FINAL DE CLASE

--- balanceo.js
+++ balanceo.js
@@ -3,21 +3,27 @@ function esBalanceada(expresion){
     let pares = {
         ")": "(",
         "]": "[",
-        "}": "{"}; //diccionario de pares
+        "}": "{",
+        ">": "<"
+    }; // diccionario de pares
     
     for (let caracter of expresion){
-        if (["(", "[", "{"].includes(caracter)){
+        if (["(", "[", "{", "<"].includes(caracter)){
             pila.push(caracter);
-        } else if ([")", "]", "}"].includes(caracter)){
+        } else if ([")", "]", "}", ">"].includes(caracter)){
             if (pila.length === 0 || pila.pop() !== pares[caracter]){
                 return false;
             }
         }
     }
-return pila.length === 0;
+    return pila.length === 0;
 }
 
-//pruebas
-console.log(esBalanceada("{[()]}")); //true
-console.log(esBalanceada("{[(])}")); //false
-console.log(esBalanceada("((()))")); //true
+// pruebas
+console.log(esBalanceada("{[()]}")); // true
+console.log(esBalanceada("{[(])}")); // false
+console.log(esBalanceada("((()))")); // true
+console.log(esBalanceada("<{[()]}>")); // true
+console.log(esBalanceada("<{[(])}>")); // false
+console.log(esBalanceada("<<>>")); // true
+console.log(esBalanceada("<{[}>")); // false
